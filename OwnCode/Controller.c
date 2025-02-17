fbf805556a7be29b4c4388d893db9541abcd786e
Sakul <lukasoboy5@gmail.com>
2025-02-17 12:27:23 +0100

    Simulated another

--- OwnCode/Controller.c
+++ OwnCode/Controller.c
@@ -1,7 +1,9 @@
 //
 //int omega =  k_PHI * (goal_theta - theta);
 
-right = (int)(k_PHI * (goal_theta - theta) * 0.5);
+//Roation
+//right = (int)(k_PHI * (goal_theta - theta) * 0.5);
+right = 
 std::cout << left;
 std::cout << "\n";
 //right = (int)((float)omega * -0.5);
--- OwnCode/OwnVariables.c
+++ OwnCode/OwnVariables.c
@@ -2,8 +2,9 @@ int h = 1;
 
 double R = 0.033;
 double L = 0.160;
-double k_PHI = (float)(2*L/(h*R));
 
 double k_PHI = (float)(2*L/(h*R));
+double k_omega = (float)(2/(H*R));
 std::cout << "K_PHI: " << k_PHI << "\n";
 int goal_theta = 90;
+int goal_d0 = 60;
