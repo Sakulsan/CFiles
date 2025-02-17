cff98ecdeb05c762a8e7907a6b751f8423bf5bf0
Sakul <lukasoboy5@gmail.com>
2025-02-17 13:12:13 +0100

    Task 8

--- OwnCode/Controller.c
+++ OwnCode/Controller.c
@@ -1,12 +1,11 @@
-//
-//int omega =  k_PHI * (goal_theta - theta);
-
 //Roation
-//right = (int)(k_PHI * (goal_theta - theta) * 0.5);
-v = k_omega * (cos(theta)*(x0-x)+sin(theta)*(y0-y));
+d_theta = (int)(k_PHI * (goal_theta - theta) * 0.5);
+//right = d_theta;
+//left = -d_theta;
+
+v = (int)(k_omega * (cos(theta* PI /180)*(x0-x)+sin(theta* PI /180)*(y0-y)));
 left = v;
 right = v;
+
 std::cout << left << " | " << (x0-x);
 std::cout << "\n";
-//right = (int)((float)omega * -0.5);
-//left = -right;
--- OwnCode/OwnVariables.c
+++ OwnCode/OwnVariables.c
@@ -4,8 +4,9 @@ double R = 0.033;
 double L = 0.160;
 
 double k_PHI = (float)(2*L/(h*R));
-double k_omega = (float)(0.5*2/(h*R));
+double k_omega = (float)(0.4*2/(h*R));
 std::cout << "K_PHI: " << k_PHI << "\n";
 std::cout << "K_omega: " << k_omega << "\n";
 int goal_theta = 90;
+int d_theta = 0;
 int v = 0;
