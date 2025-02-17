b5a5648455728ce87b2076128b434ace7dfb8130
Sakul <lukasoboy5@gmail.com>
2025-02-17 14:30:10 +0100

    Task 9-13

--- OwnCode/Controller.c
+++ OwnCode/Controller.c
@@ -1,11 +1,28 @@
-//Roation
-d_theta = (int)(k_PHI * (goal_theta - theta) * 0.5);
-//right = d_theta;
-//left = -d_theta;
+//Roational (Task 6)
+omega = (int)(k_PHI * (goal_theta - theta) * 0.5);
+//right = omega;
+//left = -omega;
 
-v = (int)(k_omega * (cos(theta* PI /180)*(x0-x)+sin(theta* PI /180)*(y0-y)));
-left = v;
-right = v;
+//Transitional (Task 7)
+v = (int)(k_omega * (cos(theta* PI /180)*(xg-x)+sin(theta* PI /180)*(yg-y)));
+//left = v;
+//right = v;
+
+//Both (Task 9)
+right = v + omega*0.5;
+left = v - omega*0.5;
 
 std::cout << left << " | " << (x0-x);
 std::cout << "\n";
+
+//
+//d_g = (int)(k_omega_2 * (cos(theta* PI /180)*(xg-x)+sin(theta* PI /180)*(yg-y)));
+
+theta_g = atan2(yg-y,xg-x);
+std::cout << "Theta_g" << " | " << d_g << "\n";
+
+//Not used (bad approximation?)
+d_p=p*(theta_g-theta);
+omega = (int)(k_PHI_2 * d_p);
+//right = omega * 0.5;
+//left = -omega * 0.5;
--- OwnCode/OwnVariables.c
+++ OwnCode/OwnVariables.c
@@ -2,11 +2,17 @@ int h = 1;
 
 double R = 0.033;
 double L = 0.160;
+double p = 2; //Idk about what p should be
 
-double k_PHI = (float)(2*L/(h*R));
-double k_omega = (float)(0.4*2/(h*R));
+double k_PHI = (float)(0.5*2*L/(h*R));
+double k_omega = (float)(0.5*2/(h*R));
+double k_omega_2 = (float)(0.5*2/(h*R));
+double k_PHI_2 = (float)(0.5*2*L/(p*h*R));
 std::cout << "K_PHI: " << k_PHI << "\n";
 std::cout << "K_omega: " << k_omega << "\n";
 int goal_theta = 90;
-int d_theta = 0;
+int omega = 0;
+int theta_g = 0;
 int v = 0;
+int d_g = 0;
+int d_p = 0;
