7101e1dd50fcfb2069694b203d315b8e6c53e7fe
Sakul <lukasoboy5@gmail.com>
2025-02-17 15:25:06 +0100

    Finished the code for task 15

--- OwnCode/Controller.c
+++ OwnCode/Controller.c
@@ -1,28 +1,32 @@
 //Roational (Task 6)
-omega = (int)(k_PHI * (goal_theta - theta) * 0.5);
-//right = omega;
-//left = -omega;
+omega = (int)(k_PHI * (goal_theta - theta));
+//right = omega*0.5;
+//left = -omega*0.5;
 
 //Transitional (Task 7)
-v = (int)(k_omega * (cos(theta* PI /180)*(xg-x)+sin(theta* PI /180)*(yg-y)));
+v = (int)(k_omega * (cos(theta* PI /180)*(x0-x)+sin(theta* PI /180)*(y0-y)));
 //left = v;
 //right = v;
 
 //Both (Task 9)
-right = v + omega*0.5;
-left = v - omega*0.5;
+//right = v + omega*0.5;
+//left = v - omega*0.5;
 
 std::cout << left << " | " << (x0-x);
 std::cout << "\n";
 
 //
-//d_g = (int)(k_omega_2 * (cos(theta* PI /180)*(xg-x)+sin(theta* PI /180)*(yg-y)));
+d_g = (int)(k_omega_2 * (cos(theta* PI /180)*(xg-x)+sin(theta* PI /180)*(yg-y)));
 
-theta_g = atan2(yg-y,xg-x);
-std::cout << "Theta_g" << " | " << d_g << "\n";
+theta_g = 180*(atan2(yg-y,xg-x))/PI;
+std::cout << "Theta_g" << " | " << theta_g << "\n";
 
-//Not used (bad approximation?)
+//Not used (bad approximation?) (Task 14)
 d_p=p*(theta_g-theta);
 omega = (int)(k_PHI_2 * d_p);
 //right = omega * 0.5;
-//left = -omega * 0.5;
+//left =  omega * 0.5;
+
+//Full controller (Task 15)
+right = d_g + omega * 0.5;
+left =  d_g - omega * 0.5;
