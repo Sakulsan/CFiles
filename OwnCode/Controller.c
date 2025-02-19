429ac3cdd041be0144382c19dc72f99a184ef8f3
Sakul <lukasoboy5@gmail.com>
2025-02-19 15:19:08 +0100

    push

--- OwnCode/Controller.c
+++ OwnCode/Controller.c
@@ -12,44 +12,55 @@
 //right = v + omega*0.5;
 //left = v - omega*0.5;
 
-std::cout << left << " | " << (x0-x);
-std::cout << "\n";
+//std::cout << left << " | " << (x0-x);
+//std::cout << "\n";
+
+std::cout << "u_l" << " | " << left << "\n";
 
 theta_g = 180*atan2(yg-y0,xg-x0)/PI;
 if((theta_g > 175 || theta_g < -175) && theta < -135) {std::cout << "Compensating" << "\n"; theta_left_comp = theta + 360;}
  else {theta_left_comp = theta;}
-//d_g = (int)(k_omega_2 * (cos(PI*theta_g/180)*(xg-x)+sin(PI*theta_g/180)*(yg-y)));
+
+d_g = (int)(k_omega_2 * (cos(PI*theta_g/180)*(xg-x)+sin(PI*theta_g/180)*(yg-y)));
+//right = d_g;
+//left = d_g;
 
 
-std::cout << "Theta_g" << " | " << theta_g << "\n";
 
-//Not used (bad approximation?) (Task 14)
-d_p = (int)(k_PHI_2 * p * (theta_g-theta_left_comp));
-//omega = (int)(k_PHI * (goal_theta - theta));
 
-right = d_p * 0.5;
-left =  -d_p * 0.5;
+//d_p = (int)(k_PHI_2 * p * (theta_g-theta_left_comp));
+d_p = (sin(PI*theta_g/180)*(x+p*cos(PI*theta_left_comp/180)-x0))   -  (cos(PI*theta_g/180)*(y+p*sin(PI*theta_left_comp/180)-y0));
+
+std::cout << "d_p" << " | " << d_p << "\n";
+
+omega = (int)(k_PSI_2 * d_p);
+
+
+
+right = d_g + omega * 0.5;
+left =  d_g - omega * 0.5;
 
 //Full controller (Task 15)
 //right = d_g + omega * 0.5;
 //left =  d_g - omega * 0.5;
 
 /*
-
 //Transitions G
 switch (current_state) {
  case rotational:
-   if (abs(d_p) < 1) current_state = transitional;
+   if (abs(d_p) < 1) {current_state = transitional;
+     std::cout << "stopped rotating" << "\n";
+   }
    break;
  case transitional:
-   if(abs(d_g) < 10 ) current_state = idle;
+   if(abs(d_g) < 10 ) {
+     current_state = idle;
+     std::cout << "Stopped transitioning\n";
+   }
    break;
  case idle:
    break;
  }
-//Used to se if we change goals and should change direction
-xg_old = xg;
-yg_old = yg;
 
 
 //controller functions f
--- OwnCode/OwnVariables.c
+++ OwnCode/OwnVariables.c
@@ -2,28 +2,27 @@ int h = 1;
 
 double R = 0.033;
 double L = 0.160;
-double p = 2; //Idk about what p should be
+double p = 100; //Idk about what p should be
 
 double k_PHI = (float)(0.5*2*L/(h*R));
 double k_omega = (float)(0.5*2/(h*R));
-double k_omega_2 = (float)(2/(h*R));
-double k_PHI_2 = (float)(0.1*2*L/(p*h*R));
-std::cout << "K_PHI: " << k_PHI << "\n";
-std::cout << "K_omega: " << k_omega << "\n";
+double k_omega_2 = (float)(0.5*2/(h*R));
+double k_PSI_2 = (float)(10*L/(p*h*R));
+std::cout << "K_PSI_1: " << k_PHI << "\n";
+std::cout << "K_omega_1: " << k_omega << "\n";
+std::cout << "K_PSI_2: " << k_PSI_2 << "\n";
+std::cout << "K_omega_2: " << k_omega_2 << "\n";
 int goal_theta = 0;
 int omega = 0;
 double theta_g = 0;
 int theta_left_comp = 0;
 int v = 0;
 int d_g = 0;
-int d_p = 0;
+double d_p = 0;
 
 //The state Q
 enum movement_states { idle , rotational , transitional };
 enum movement_states current_state = rotational;
-
-int xg_old = xg; //Used for tranition out of "Idle"
-int yg_old = yg;
 
 //Possible tuple/struct?
 int points_x [] = {0,0,1};
